9cf162e15b7bd959546dd071bb675e0bc41b0031
devUserContct <developer.user.contact@protonmail.com>
2023-04-10 18:57:27 -0700

    added landing page component

---
+++ components/LandingPage/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from '../../styles/Home.module.scss'
+
+const LandingPage = () => {
+  return (
+    <>
+      <p className={styles.titleTextSmall}>Julie W</p>
+      <p className={styles.titleTextSmall}>Shop</p>
+    </>
+  )
+}
+
+export default LandingPage
