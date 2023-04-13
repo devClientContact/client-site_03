21702cf32420e81d731f7642b6e82921182d31d0
devUserContct <developer.user.contact@protonmail.com>
2023-04-12 17:05:38 -0700

    edited styles

--- components/ProductsHeader/index.tsx
+++ components/ProductsHeader/index.tsx
@@ -4,7 +4,7 @@ import styles from '../../styles/Home.module.scss'
 const ProductsHeader = () => {
   return (
     <div className={styles.productsHeaderContainer}>
-      <p className={styles.headerText}>Julie W / </p>
+      <p className={styles.headerText}>Julie W /</p>
       <p className={styles.headerTextBold}>Products</p>
     </div>
   )
