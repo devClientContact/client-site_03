b0bcf8f401dda1aa6131bcddf3d8284710c09eac
devUserContct <developer.user.contact@protonmail.com>
2023-04-13 21:00:18 -0700

    edited styles

--- components/LandingPage/index.tsx
+++ components/LandingPage/index.tsx
@@ -5,8 +5,10 @@ import styles from '../../styles/Home.module.scss'
 const LandingPage = () => {
   return (
     <div className={styles.mainContainer}>
-      <p className={styles.titleText}>Julie W</p>
-      <p className={styles.titleText}>Shop</p>
+      <div className={styles.titleContainer}>
+        <p className={styles.titleText}>Julie W</p>
+        <p className={styles.titleText}>Shop</p>
+      </div>
       <div className={styles.navbarContainer}>
         <div className={styles.navItem}>
           <Link href='/products'>
