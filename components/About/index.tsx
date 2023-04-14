9e0dcf31edf90ef32e376f95d72461c9bc2f159a
devUserContct <developer.user.contact@protonmail.com>
2023-04-13 19:18:14 -0700

    added `About` page

---
+++ components/About/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from '../../styles/Home.module.scss'
+
+const About = () => {
+  return (
+    <div className={styles.mainContainer}>
+      <p className={styles.aboutText}>test</p>
+    </div>
+  )
+}
+
+export default About
---
+++ pages/about.tsx
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+import About from '@/components/About'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Julie W Shop</title>
+        <meta name='description' content='Online Shop For Julie W Merch' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <main>
+        <>
+          <About />
+        </>
+      </main>
+    </>
+  )
+}
