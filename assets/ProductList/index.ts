f6f49cc0577aa46778e5e8b2d1649a6d5fae521c
devUserContact <developer.user.contact@protonmail.com>
2023-04-17 11:13:58 -0700

    added `ProductList` interface

---
+++ assets/ProductList/index.ts
@@ -0,0 +1,20 @@
+export interface ProductList {
+  productName: string;
+  description: string;
+  imageURL: string | string[];
+  price: number;
+}
+export const products: ProductList[] = [
+  {
+    productName: "",
+    description: "",
+    imageURL: "",
+    price: 0.00,
+  },
+  {
+    productName: "",
+    description: "",
+    imageURL: "",
+    price: 0.00,
+  },
+];
