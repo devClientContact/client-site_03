2cfb268644accd182eb5ab2c2acc6268ed1dc235
devUserContct <developer.user.contact@protonmail.com>
2023-04-20 22:36:53 -0700

    edited variable names in project and developed product grid

--- assets/ProductList/index.ts
+++ assets/ProductList/index.ts
@@ -1,18 +1,18 @@
 export interface ProductList {
-  productName: string;
+  name: string;
   description: string;
   imageURL: string | string[];
   price: number;
 }
 export const products: ProductList[] = [
   {
-    productName: "",
+    name: "testProduct01",
     description: "test01",
     imageURL: "",
     price: 0.00,
   },
   {
-    productName: "",
+    name: "testProduct02",
     description: "test02",
     imageURL: "",
     price: 0.00,
--- components/Product/index.tsx
+++ components/Product/index.tsx
@@ -6,8 +6,16 @@ const Product = ({ product }: any) => {
   const ROUTE_POST_ID = 'products/[productName]'
   return (
     <>
-      <div className={styles.gridItem}>
-        {product.description}
+      <div className={styles.gridItemContainer}>
+        <div className={styles.productImageContainer}></div>
+        <div className={styles.productTextContainer}>
+          <div className={styles.productName}>
+           {product.name}
+          </div>
+          <div className={styles.productPrice}>
+           {product.price}
+          </div>
+        </div>
       </div>
     </>
   )
